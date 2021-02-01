63f3fcf11e81308742ccbd8171cc93cb4f4dd3ac
jorgelima <jorge.lima@teknisa.com>
2021-02-01 14:44:11 -0300

    pequenas alteracoes

--- index.php
+++ index.php
@@ -16,16 +16,14 @@ include('./php/head.php');
 
 <br>
 <?php
-if (isset($_SESSION['erro']))
-{
-    if ($erro == 'sem erro')
-    {
-
-    }
-    else
-    {
-        echo '<div class="alert alert-danger" role="alert">' . $erro . '
-            </div>';
+if (isset($_SESSION['erro'])) {
+    if ($erro == 'sem erro') {
+        
+    } else {
+?>
+       <div class="alert alert-danger" role="alert"><?= $erro ?>
+           </div>
+    <?php
     }
 }
 
@@ -33,61 +31,69 @@ if (isset($_SESSION['erro']))
 <br>
 <div class="list-group">
 <?php
-if (is_array($row))
-{
-    $arrayHeader = [];
-    foreach ($row as $key => $item)
-    {
+if (is_array($row)) {
+    $arrayHeader = array();
+    foreach ($row as $key => $item) {
         array_push($arrayHeader, $key);
-    };
-
+    }
+    ;
+    
 ?>
-       <div id="tabela">
+      <div id="tabela">
        <table class='table table-dark' id="table">
                 <thead>
                     <tr>
                     <th scope='col'>#</th>
                     <?php
-    foreach ($arrayHeader as $headerItem)
-    {
-
-        if ($headerItem)
-        {
-            echo "<th scope='col' style='text-align: center'>" . $headerItem . "</th>";
-        }
-        else
-        {
-            echo "<th scope='col' style='text-align: center'> NULL </th>";
-        }
-    }
-    echo "</thead>";
-    echo "<tbody> <tr>";
-
-    $sizeRow = 0;
-
-    for ($i = 0;$i <= $sizeRow;$i++)
-    {
-        echo "<td>" . $i . "</td>";
-
-        foreach ($row as $item)
-        {
-            $sizeRow = sizeof($item);
-            if (isset($item[$i]))
-            {
-                echo "<td scope='col' style='text-align: center'>" . $item[$i] . "</td>";
-            }
-            else
-            {
-                echo "<td scope='col' style='text-align: center'> -- </td>";
-            }
-        };
-        echo '</tr>';
-    }
-    echo '</thead>';
-    echo '</table>';
-};
-
+    foreach ($arrayHeader as $headerItem) {
+        
+        if ($headerItem) {
 ?>
+           <th scope='col' style='text-align: center'><?= $headerItem ?>  </th>
+        <?php
+        } else {
+?>
+       
+            <th scope='col' style='text-align: center'> NULL </th>
+            
+        <?php
+        }
+    }
+?>
+   </thead>
+    <tbody> <tr>
+
+<?php
+    $sizeRow = 0;
+    
+    for ($i = 0; $i <= $sizeRow; $i++) {
+        echo "<td>" . $i . "</td>";
+        
+        foreach ($row as $item) {
+            $sizeRow = sizeof($item);
+            if (isset($item[$i])) {
+?>
+               <td scope='col' style='text-align: center'> <?= $item[$i] ?> </td>
+            <?php
+            } else {
+?>
+               <td scope='col' style='text-align: center'> -- </td>
+            <?php
+            }
+        }
+        ;
+?>
+       </tr>;
+    <?php
+    }
+?>
+   </thead>
+    </table>
+<?php
+}
+;
+?>
+
 </tr>
   </tbody>
   </table>
@@ -96,5 +102,5 @@ if (is_array($row))
 </body>
 
 <?php
-    include('./php/scripts.php');
+include('./php/scripts.php');
 ?>
